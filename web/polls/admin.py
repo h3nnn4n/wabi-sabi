8c396d604742a0c310b6d9c667788d34a47ae978
h3nnn4n <uber.renan@gmail.com>
2020-09-23 19:25:13 -0300

    Tune css + admin

--- web/polls/admin.py
+++ web/polls/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
