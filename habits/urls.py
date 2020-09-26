9b9494e08cf22e2fc772f9b12b42ffaddb323a2d
h3nnn4n <uber.renan@gmail.com>
2020-09-26 19:00:00 -0300

    Allow creating new habits

--- habits/urls.py
+++ habits/urls.py
@@ -7,4 +7,6 @@ app_name = 'habits'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<uuid:pk>/', views.HabitView.as_view(), name='detail'),
+    path('new/', views.HabitNewView.as_view(), name='new_habit'),
+    path('create/', views.create_habit, name='create_habit'),
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,4 +1,10 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
+
+from psycopg2.errors import NotNullViolation
 
 from .models import Habit
 
@@ -26,3 +32,30 @@ class IndexView(generic.ListView):
 class HabitView(generic.DetailView):
     model = Habit
     template_name = 'habits/habit_detail.html'
+
+
+class HabitNewView(generic.CreateView):
+    model = Habit
+    template_name = 'habits/habit_new.html'
+    fields = [
+        'name',
+        'public',
+    ]
+
+
+def create_habit(request):
+    habit = None
+
+    try:
+        habit = Habit(user=request.user)
+        habit.name = request.POST['name']
+        habit.public = request.POST['public'] == 'on'
+        habit.created_at = timezone.now()
+        habit.save()
+    except (NotNullViolation, KeyError):
+        return render(request, 'habits/habit_new.html', {
+            'habit': habit,
+            'error_message': 'Error while creating Habit',
+        })
+    else:
+        return HttpResponseRedirect(reverse('habits:detail', args=(habit.id,)))
